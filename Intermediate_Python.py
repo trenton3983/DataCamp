4fe67cdf3057704151dbefcd07cd6126a35cafe2
Trenton J. McKinney <trenton_mckinney@yahoo.com>
2018-06-10 17:58:54 -0700

    additional examples

--- Intermediate_Python.py
+++ Intermediate_Python.py
@@ -303,4 +303,23 @@ def ex_19():
     print(cars)
 
 
-ex_19()
+def ex_20():
+    # Build cars DataFrame
+    names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+    dr = [True, False, False, False, True, True, True]
+    cpc = [809, 731, 588, 18, 200, 70, 45]
+    dict_1 = {'country': names, 'drives_right': dr, 'cars_per_cap': cpc}
+    cars = pd.DataFrame(dict_1)
+    print(cars)
+
+    # Definition of row_labels
+    row_labels = ['US', 'AUS', 'JAP', 'IN', 'RU', 'MOR', 'EG']
+
+    # Specify row labels of cars
+    cars.index = row_labels
+
+    # Print cars again
+    print(cars)
+
+
+ex_20()
