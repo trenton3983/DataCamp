f32ab65dc3112c4b077d84a6b7742c541b93c5af
Trenton J. McKinney <trenton3983@users.noreply.github.com>
2018-06-12 17:30:12 -0700

    added lessons for enumerate and zip

--- python_data_science_toolbox_2.py
+++ python_data_science_toolbox_2.py
@@ -5,6 +5,44 @@
 # finish the chapter with a use case that is pertinent to the world of Data Science - dealing with large amounts of
 # data - in this case, data from Twitter that you will load in chunks using iterators!
 
+avengers = ['hawkeye', 'iron man', 'thor', 'quicksliver']
+names = ['barton', 'stark', 'odinson', 'maximoff']
+
+
+def lesson_enumerate():
+    print('Part 1:')
+    e = enumerate(avengers)
+    print(type(e))
+    e_list = list(e)
+    print(e_list)
+    print('\nPart 2:')
+    for i, v in enumerate(avengers, start=10):
+        print(i, v)
+
+
+print('Output of lesson_enumerate:')
+lesson_enumerate()
+
+
+def lesson_zip():
+    print('Part 1:')
+    z = zip(avengers, names)
+    print(type(z))
+    z_list = list(z)
+    print(z_list)
+
+    print('\nPart 2:')
+    for z1, z2 in zip(avengers, names):
+        print(z1, z2)
+
+    print('\nPart 3:')
+    z = zip(avengers, names)  # z has to be repacked because the iter object was used in part 1
+    print(*z)
+
+
+print('\nOutput of lesson_zip:')
+lesson_zip()
+
 # List Comprehensions and Generators
 
 # In this chapter, you'll build on your knowledge of iterators and be introduced to list comprehensions, which allow
