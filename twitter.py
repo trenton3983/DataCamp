73eea6634548f14289118d7f8ab9544a9e172597
Trenton J. McKinney <trenton3983@users.noreply.github.com>
2018-06-28 13:47:09 -0700

    initial commit

---
+++ twitter.py
@@ -0,0 +1,21 @@
+import tweepy
+import json
+
+
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api=None):
+        super(MyStreamListener, self).__init__()
+        self.num_tweets = 0
+        self.file = open("tweets.txt", "w")
+
+    def on_status(self, status):
+        tweet_list = []
+        tweet = status._json
+        self.file.write(json.dumps(tweet) + '\n')
+        tweet_list.append(status)
+        self.num_tweets += 1
+        if self.num_tweets < 100:
+            return True
+        else:
+            return False
+        self.file.close()
