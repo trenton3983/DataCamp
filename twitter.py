730b696a0049bf6ea9d6a1276d91b0ac69f3502a
Trenton J. McKinney <trenton_mckinney@yahoo.com>
2018-06-28 19:16:20 -0700

    modified class from DataCamp to a working version

--- twitter.py
+++ twitter.py
@@ -9,13 +9,14 @@ class MyStreamListener(tweepy.StreamListener):
         self.file = open("tweets.txt", "w")
 
     def on_status(self, status):
-        tweet_list = []
         tweet = status._json
         self.file.write(json.dumps(tweet) + '\n')
-        tweet_list.append(status)
         self.num_tweets += 1
-        if self.num_tweets < 100:
+        if self.num_tweets < 1000:
             return True
         else:
             return False
         self.file.close()
+
+    def on_error(self, status):
+        print(status)
